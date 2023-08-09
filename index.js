670450854b52cb596252e9c62f11dcd65a01e46c
bappy-ahashan <bappyahashan@gmail.com>
2023-08-10 03:38:10 +0600

    set the post,get,delete and set the jwt and use the token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
@@ -27,6 +28,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(403).send({ error: true, message: 'unauthorized people' });
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ error: true, message: 'unauthorized people' });
+        }
+
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -36,6 +54,26 @@ async function run() {
         const checkoutcollection = client.db('carsMaster').collection('checkout');
 
 
+
+        // jwt token start.........................
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+
+        })
+
+
+
+
+
+        // jwt token end.........................
+
+
+
+
+        // services...............start
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find();
             const result = await cursor.toArray();
@@ -50,7 +88,14 @@ async function run() {
 
         })
 
-        app.get('/checkout', async (req, res) => {
+        // services**********end
+
+
+        // checkout/booking................start
+
+        app.get('/checkout', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email };
@@ -66,6 +111,21 @@ async function run() {
 
         })
 
+
+        app.patch('/checkout/:id', async (req, res) => {
+            const id = req.params.id;
+            const confirm = req.body;
+
+            const filter = { _id: new ObjectId(id) };
+            updatedDoc = {
+                $set: {
+                    status: confirm.status
+                }
+            }
+            const result = await checkoutcollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
         app.delete('/checkout/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -74,7 +134,7 @@ async function run() {
         })
 
 
-
+        // checkout/booking.....................end
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
